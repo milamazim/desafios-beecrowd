a450f48d9455db2e950bfd31e1010b00ccf6cfaf
Camila Marques <milamazim@gmail.com>
2025-07-27 19:06:24 -0300

    add: implement logic to find the largest of three numbers

---
+++ 1013-o-maior.py
@@ -0,0 +1,14 @@
+a,b,c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+
+maior_ab = (a+b+abs(a-b))/2
+maior_abc = (maior_ab+c+abs(maior_ab-c))/2
+
+if maior_ab > maior_abc: 
+    maior = int(maior_ab)
+else: 
+    maior = int(maior_abc)
+
+print(f'{maior} eh o maior')
